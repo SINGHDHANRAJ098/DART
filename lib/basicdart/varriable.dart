6177e58fcb8e80ac6c096ed470bdf72f232ffd44
dhanraj <singhdhanraj225@gmail.com>
2025-12-18 23:08:35 +0530

    data type varriable

--- lib/basicdart/varriable.dart
+++ lib/basicdart/varriable.dart
@@ -1,10 +1,65 @@
+// void main (){
+//   // variable in dart
+//   int number; // how to delcare variable
+//   number = 40;
+//   int numbertwo;
+//   numbertwo = 9;
+//   int numberthree;
+//   numberthree = number * numbertwo;
+//   print(numberthree);
+// }
+
+
+  //  int a = 33;
+  //  int b =9;
+  // print(a.runtimeType);
+  // int result;
+  // result= a+b;
+  // result=a-b;
+  // result=a*b;
+  // print(result);
+
+
+  //swap value
+
+  // int a=120;
+  // int b =13;
+  // int x;
+  // a = a+b;
+  // b=a-b;
+  // a=a-b;
+  // print("value of a: $a");
+  // print("value of b: $b");
+
+///factorial
+
+// import 'dart:io';
+//
+// void main() {
+//   print("Enter a number:");
+//   int n = int.parse(stdin.readLineSync()!);
+//
+//   int factorial = 2;
+//   for (int i = 2; i <= n; i++) {
+//     factorial *= i;
+//   }
+//
+//   print(factorial);
+// }
+
+
+// void main (){
+//   String a = 'dhanraj';
+//
+//   String reverse = a.split("").reversed.join("");
+//   print(reverse);
+//
+// }
+
 void main (){
-  // variable in dart
-  int number; // how to delcare variable
-  number = 40;
-  int numbertwo;
-  numbertwo = 9;
-  int numberthree;
-  numberthree = number * numbertwo;
-  print(numberthree);
+  int a =10;
+  int b =30;
+  bool result;
+  result = (b >a);
+  print(result);
 }
