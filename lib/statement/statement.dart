e1ecb2425c564f785d105c0edc778c0894e53a48
dhanraj <singhdhanraj225@gmail.com>
2025-12-31 13:46:12 +0530

    loops

---
+++ lib/statement/statement.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+/// if - else
+
+// void main() {
+//   int age = 10;
+//
+//   if (age >= 18) {
+//     print("ADUILT");
+//   } else {
+//     print("child");
+//   }
+// }
+
+
+
+
+
+/// for loop
+
+
+
+
+/// using for loop print table
+
+
+
+
+// void main() {
+//
+//   int x = 4;
+//   for (int i = 1; i <= 10; i++) {
+//     print('$x * $i = ${x*i}');
+//   }
+// }
+
+
+
+/// even odd
+//
+// void main() {
+//
+//
+//   for (int i = 1; i <= 10; i++) {
+//     if (i %2 ==0){
+//       print('even');
+//     }else{
+//       print("odd");
+//     }
+//   }
+// }
+
